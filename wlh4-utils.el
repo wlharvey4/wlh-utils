50226b3ac160da77ba6fb367c839f5f3b1a2d949
wlharvey4 <wlharvey4@mac.com>
2021-03-15 13:04:09 -0700

    Refactored to include def start and end, and desc start and end

--- wlh4-utils.el
+++ wlh4-utils.el
@@ -2,8 +2,8 @@
 
 ;;; Author: wlh4
 ;;; Initial Commit: 2021-03-10
-;;; Time-stamp: <2021-03-15 00:05:30 lolh-mbp-16>
-;;; Version: 0.1.7
+;;; Time-stamp: <2021-03-15 13:04:02 minilolh>
+;;; Version: 0.1.8
 
 ;;; Commentary:
 
@@ -12,24 +12,31 @@
 
 ;;; TODO:
 ;;   - Calculate usages
+;;   - Add defvar default value if exists
+;;   - Print only first line of description;
+;;     rest of description is invisible but clickable
+;;     to reveal.
 
 ;;; Code:
 
 ;; wlh4-defs: plist of wlh4-defnm structs, sorted alphabetically
-;; wlh4-defnm: cl-struct: :name :desc :args :file :start :end :usages
+;; wlh4-defnm: cl-struct: :name :desc :desc-st :desc-en :args :file :start :end :usages
 ;; :name symbol
 ;; :desc string
+;; :desc-st point
+;; :desc-en point
 ;; :args string TODO: turn into a list of symbols
 ;; :file string
-;; :start point of description
-;; :end point of description
+;; :start point of def
+;; :end point of def
 ;; :usages: list of point-or-marker's
 
 (require 'cl-lib)
 (require 'seq)
+
 (defvar wlh4-defs "Property list of defs and defnm's")
-(cl-defstruct wlh4-defnm name args desc file start end usages)
-(defconst wlh4--dash "\n----------------------------------------------------------------------\n")
+(cl-defstruct wlh4-defnm name args desc desc-st desc-en file start end usages)
+(defconst wlh4--dash "\n---------------------------------------------------------------------")
 
 (defun wlh4-parse-defs (&optional buf)
   "Parse buffer `buf' (default current buffer) for all defines.
@@ -46,8 +53,13 @@ variable wlh4-defs, which other functions can reference and use."
       (while
 	  ;; find all `defines' in the buffer
 	  (search-forward-regexp "^(def" nil t)
-	(let* ((def (symbol-at-point)) ; definition type as symbol
+	(let* ((desc-st) (desc-en)
+               (def (symbol-at-point)) ; definition type as symbol
 	       (st (line-beginning-position))
+               (en (save-excursion
+                     (backward-up-list)
+                     (forward-list)
+                     (point)))
 	       (nm (progn
 		     (skip-chars-forward "^[[:space:]]")
 		     (forward-char 2)
@@ -60,14 +72,15 @@ variable wlh4-defs, which other functions can reference and use."
 			       (s (progn (backward-list)(point))))
 			   (buffer-substring-no-properties s e))
 		       "")) ; return empty string if there are no arguments
-	       (desc (progn ; 
+	       (desc (progn
 		       (forward-line)
 		       (if (looking-at "^[[:space:]]+[\"]")
-			   (let ((b (car (match-data))))
+			   (let ((b (car (match-data)))
+                                 (desc-st (line-beginning-position)))
 			     (forward-sexp)
-			     (concat "\n" (buffer-substring-no-properties b (point))))
-			 "")))
-	       (en (line-end-position)))
+                             (setq desc-en (point))
+			     (buffer-substring-no-properties b (point)))
+			 ""))))
 	       ;; TODO: Find all usages at this point
 	  (setf wlh4-defs
 		(plist-put wlh4-defs def
@@ -75,6 +88,8 @@ variable wlh4-defs, which other functions can reference and use."
 			    (make-wlh4-defnm :name nm
 					     :args args
 					     :desc desc
+                                             :desc-st desc-st
+                                             :desc-en desc-en
 					     :file buffer-file-name
 					     :start st
 					     :end en)
@@ -85,7 +100,7 @@ variable wlh4-defs, which other functions can reference and use."
 
 (defun wlh4-defs (&optional buf)
   "Parse a file for defs, then print them sorted and categorized."
- 
+
   (interactive) ; TODO: allow for arbitrary buffer
   (with-output-to-temp-buffer "tempbuf"
     (wlh4-parse-defs buf)
@@ -102,9 +117,13 @@ variable wlh4-defs, which other functions can reference and use."
 	  ;; TODO: create hidden folded descriptions
 	  ;; that unfold at a touch.
 	  ;; TODO: create hideen lists of usages that unfold
-	  (princ (format "  %s %s %d--%d\n"
+	  (princ (format ">  %s %s [%d--%d]%s\n"
 			 (wlh4-defnm-name defnm)
 			 (wlh4-defnm-args defnm)
 			 (wlh4-defnm-start defnm)
-			 (wlh4-defnm-end defnm)))))
+			 (wlh4-defnm-end defnm)
+                         (let ((desc (wlh4-defnm-desc defnm)))
+                           (if (not (string-empty-p desc))
+                               (concat "\n" desc wlh4--dash)
+                             ""))))))
       (setf defs (cddr defs)))))
