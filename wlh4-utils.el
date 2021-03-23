5c5c20bb95bb15ae9bd777839cf4df08c68e9bbc
Wesley Harvey <wlharvey4@mac.com>
2021-03-23 07:37:54 -0700

    Added function preorder-traversal to walk an OrgTree.

--- wlh4-utils.el
+++ wlh4-utils.el
@@ -2,13 +2,15 @@
 
 ;; Author: wlh4
 ;; Initial Commit: 2021-03-10
-;; Time-stamp: <2021-03-17 00:00:52 lolh-mbp-16>
-;; Version: 0.1.10
+;; Time-stamp: <2021-03-23 07:37:42 lolh-mbp-16>
+;; Version: 0.2.1
 
 ;;; Commentary:
 
 ;;  Print  all  defined  symbols  alphabetically  in  a  buffer,  with
 ;;  parameters, descriptions, and positions in file.
+
+;; Procedure to walk an org tree using the preorder traversal method
 
 ;;; TODO:
 ;;   - Calculate usages
@@ -146,5 +148,76 @@ reference and use."
                                (concat "\n" desc wlh4--dash)
                              ""))))))
       (setf defs (cddr defs)))))
+
+
+;;; Walk an Org Tree using the Preorder Traversal method
+;;  See https://opendsa-server.cs.vt.edu/ODSA/Books/CS3/html/GenTreeIntro.html
+
+(defun init-org-tree (buf)
+  "Obtain an OrgTree data structure for a buffer."
+  (with-current-buffer buf
+    (org-element-parse-buffer)))
+
+(defun _prop-keys (props)
+  "Create a string of key symbols from a plist."
+  (let ((prop-str ""))
+    (while props
+      (let ((key (symbol-name (pop props))))
+	(setf prop-str (format "%s %s" prop-str key))
+	(pop props)))
+    prop-str))
+
+;;; Preporder Traversal of a General Tree
+;;  https://opendsa-server.cs.vt.edu/ODSA/Books/CS3/html/GenTreeIntro.html
+(defun preorder-traversal (org-node level)
+  "Performs a preorder traversal of an OrgTree.
+
+An OrgTree node is a list containing a `type' designator, a plist
+of properties relevant  to the type, and an  indefinite number of
+child OrgTree  nodes.  
+
+  OrgTree node: (<type> (plist ...) (child node) (child node) ...)
+
+The key to traversing an OrgTree is knowing that the Org function
+`org-element-contents' returns a list of  child nodes if they are
+present, which list  can be traversed recursively.   If there are
+no child  nodes, then this function  does not return a  list, and
+no recursion should take place.
+
+Also, sometimes the org-node is not a readable list, and so it is
+not possible to return a list of properties, even though there is
+a type.
+
+This function  simply prints some relevant  information about the
+current  node.  It  keeps track  of the  level, prints  the level
+number, and indents  the information by `level'  spaces.  It also
+prints a raw value or value  if one is present in the properties.
+Finally,  it prints  a string  of  keys from  the plist  (without
+values) for reference purposes."
+  (let* ((type (org-element-type org-node))
+	 (props (and (listp org-node) (second org-node)))
+	 (contents (org-element-contents org-node))
+	 (raw-val (plist-get props :raw-value))
+	 (val (plist-get props :value)))
+    (princ
+     (format "%2d]%s%s[%s]: %s \n"
+	     level
+	     (make-string level 32)
+	     type
+	     (if raw-val
+		 (format "\"%s\""raw-val)
+	       (if val (format "\"%s\"" val) ""))
+	     (_prop-keys props)))
+    (if (listp  contents); Only  continue and recurse if  org-node is
+			 ; not a leaf node (i.e. has no child nodes.)
+	(let ((child (first contents))
+	      (children (rest contents)))
+	  (while child
+	    (preorder-traversal child (1+ level))
+	    (setf child (first children))
+	    (setf children (rest children))))))
+  t)
+
+;; USAGE: (preorder-traversal (init-org-tree "walk.org") 0)
 
 ;;; wlh4-utils.el ends here
